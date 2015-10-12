e0aa6322b3da87c60e750334f3650687b30dc61d
emmazzz <emmazhong@cmu.edu>
2015-10-12 01:44:15 -0400

    draw cards

--- exercises/sort_the_shapes/main.js
+++ exercises/sort_the_shapes/main.js
@@ -97,11 +97,70 @@ var main = function(ex) {
 		};
 		return code;
 	}
+	var card_color = [48,144,255];
+	var up_margin = 40;
+	var margin = 20;
+	var side_margin = 10;
+	var total_width = ex.width()/2 - side_margin*2;
+	var total_height = ex.height() - side_margin*2;
 
+	function Card(level,level_count){
+		var card = {};
+		card.rvalue = undefined;
+		card.last_return = undefined;
+		card.level = level;
+		card.x = side_margin + ex.width()/2+ (card.level-1)*side_margin;
+		card.y = side_margin + (card.level-1)*up_margin;
+		card.width = total_width - (card.level-1)*side_margin*2;
+		card.height = total_height - (card.level-1)*(up_margin + margin);
+		card.r = (card_color[0]*(level_count-card.level+1)/level_count).toString(16);
+		card.g = ((card_color[1]*(level_count-card.level+1))/level_count).toString(16);
+		card.b = ((card_color[2]*(level_count-card.level+1))/level_count).toString(16);
+
+		card.pop_up = function(){};
+
+		card.vanish = function(){};
+
+		card.draw = function(){
+			ex.graphics.ctx.fillStyle = "#"+card.r+card.g+card.b;
+            ex.graphics.ctx.fillRect(card.x,card.y,card.width,card.height);
+            console.log(card.r,card.g,card.b);
+            console.log(ex.graphics.ctx.fillStyle);
+            console.log(card.level,card.x,card.y,card.width,card.height);
+		};
+
+		return card;
+	}
+
+	function Cards(all_cards){
+		var cards = {};
+		cards.count = all_cards.length;
+		cards.card_list = all_cards;
+
+		cards.degrade = function(){
+			var gone_card = cards.card_list.splice(cards.count - 1,1);
+			gone_card.vanish();
+			cards.count -= 1;
+			cards.card_list[cards.count-1].last_return = gone_card.rvalue;
+		}
+
+		cards.draw = function(){
+			for (i = 0;i < cards.count;i++){
+                var current = cards.card_list[i];
+                current.draw();
+			}
+		}
+
+		return cards;
+	}
 	var bottom_margin = 20;
 	var right_margin = 20;
 	var code = CodeWell(0, 0, ex.width()/2 - right_margin, 
 		                ex.height() - bottom_margin);
+    
+    ex.data.cards = Cards([Card(1,3),Card(2,3),Card(3,3)]);
+    ex.data.cards.draw();
+
 	code.init_steps();
 	code.draw("small");
 	code.colorCode(code.steps[code.curStep].lineNum, 1, "codeColor.png");
