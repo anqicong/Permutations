110735ea9c7138b13c0325379cd8e35040856dcc
hlycharles <hly.charles@gmail.com>
2015-10-11 20:36:57 -0400

    CodeWell and color

--- exercises/sort_the_shapes/main.js
+++ exercises/sort_the_shapes/main.js
@@ -13,8 +13,11 @@ var main = function(ex) {
 		step.func = func;
 		step.args = args;
 		step.call = function () {
-			func(args);
+			if(func != undefined && args != undefined) {
+				func(args);
+			}
 		}
+		return step;
 	}
 
 	function CodeWell(left, top, w, h, inputList){
@@ -25,9 +28,12 @@ var main = function(ex) {
 		code.h = h;
 		code.steps = [];
 		code.curStep = 0;
+		code.curStepImage = undefined;
 
 		code.init_steps = function() {
-			code.steps = [];
+			var step = Step(0, undefined, undefined);
+			var step2 = Step(9, undefined, undefined);
+			code.steps = [step, step2];
 		}
 
 		code.totalStep = function () {
@@ -37,25 +43,46 @@ var main = function(ex) {
 		code.nextStep = function () {
 			if(code.curStep < code.steps.length) {
 				code.curStep += 1;
-				code.steps[curStep].call();
+				code.steps[code.curStep].call();
+				code.colorCode(code.steps[code.curStep].lineNum, 1,
+					"codeColor.png");
 			}
 		};
 
 		code.draw = function (size) {
-			var bottom_margin = 15;
 			ex.createCode(left, top, ex.data.code.display, {
 				width: code.w,
-				height: code.h - bottom_margin,
+				height: code.h,
 				language: ex.data.code.language,
 				size: size
 			})
 
 		};
 
+		//Color the code starting at line start with span of span lines
+		code.colorCode = function (start, span, colorImage) {
+			if (code.curStepImage != undefined) {
+				code.curStepImage.remove();
+			}
+			var codeHeight = 14;
+			code.curStepImage = ex.createImage(0, 
+				codeHeight * (start + 1), 
+				colorImage, {
+				width: ex.width() / 2,
+				height: codeHeight * span
+			});
+			
+		};
 		return code;
 	}
 
-	var code = CodeWell(0, 0, ex.width()/2, ex.height());
-	code.draw("medium");
-	
+	var bottom_margin = 20;
+	var right_margin = 20;
+	var code = CodeWell(0, 0, ex.width()/2 - right_margin, 
+		                ex.height() - bottom_margin);
+	code.init_steps();
+	code.draw("small");
+	code.colorCode(code.steps[code.curStep].lineNum, 1, "codeColor.png");
+	ex.chromeElements.redoButton.on("click", code.nextStep);
+
 }
