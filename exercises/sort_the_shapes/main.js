80414c62dde14eda934b9084124a7b1501cc21bf
anqicong <anqicong@gmail.com>
2015-10-16 13:40:25 -0400

    initial

--- exercises/sort_the_shapes/main.js
+++ exercises/sort_the_shapes/main.js
@@ -5,5 +5,63 @@
 
 var main = function(ex) {
 
+	function State(){
+		var state = {};
+
+		state.init = function(){
+
+		};
+
+		state.draw = function(){
+
+		};
+
+		return state;
+	}
+
+	function Card(depth){
+		var card = {};
+
+		card.init = function(){
+
+		};
+
+		card.draw = function(){
+
+		};
+
+		card.checkClick = function(x, y){
+
+		};
+
+		return card;
+	}
+
+	function Line(x, y, lineNum, action){
+		var line = {};
+		line.x = x;
+		line.y = y;
+		line.lineNum = lineNum;
+		line.action = action;
+
+		line.clicked = function(x, y){
+
+		};
+
+		line.highlight = function(){
+
+		};
+
+		line.unhighlight = function(){
+
+		};
+
+		line.draw = function(){
+
+		};
+
+		return line;
+	}
+
 
 }
